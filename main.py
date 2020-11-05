02c0b05c0a4ffbfc9af9e509c647ee651686d62d
PtmCn <putthimeth_c@yahoo.com>
2020-11-05 18:11:36 +0000

    ->find xmax ymax

--- main.py
+++ main.py
@@ -5,23 +5,25 @@ side = int (input('How many sides:'))
 sidelength = int (input('How long is each side:'))
 column = int (input('How many columns:'))
 row =int  (input('How many rows:'))
-
+"""
 def find_xmax():
   k = 0
   if turtle.xcor() > k: 
     k = turtle.xcor()
   return k
+"""
 
 def draw_shape():
   turtle.forward(sidelength)
   for x in range(side-1):
     turtle.left(360/side)
     turtle.forward(sidelength)
-    find_xmax()
+    #find_xmax()
 
 ystart=0
-height = 10 #math.radians(30)*sidelength
+height = sidelength #math.radians(30)*sidelength
 gap = 10
+xmax = sidelength
 for x in range(column):
   xstart =  0
   ystart = ystart - height - gap
