e5cec4df6ec3b468d8530d3d596fed9db7e59c8e
PtmCn <69117387+PtmCn@users.noreply.github.com>
2020-11-12 20:07:10 +0700

    Add files via upload

---
+++ entity.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Graphics.hpp>
+#include "include/Entity.hpp"
+
+void Entity::draw(sf::RenderTarget& target, sf:: Renderstates states)const{
+    states.transform = m_transform;
+
+    states.texture = &m_texture;
+
+    target.draw(m_vertices, states);
+
+}
